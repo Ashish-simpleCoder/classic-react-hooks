c18fec6722af57dbbfc9b28607dcdbde17b3262d
Ashish-simpleCoder <ashishprajapati357753357@gmail.com>
2023-10-13 14:59:09 +0530

    [fix]: use-debounced-fn hook callback logic

--- src/hooks/use-debounced-fn/use-debounced-fn.tsx
+++ src/hooks/use-debounced-fn/use-debounced-fn.tsx
@@ -1,20 +1,21 @@
 import { useCallback } from 'react'
 import useSyncedRef from '../use-synced-ref/use-synced-ref'
 
-export default function useDebouncedFn<T extends (...args: any[]) => any>(cb: T, delay = 300) {
+export const DEFAULT_DELAY = 300
+
+export default function useDebouncedFn<T extends (...args: any[]) => any>(cb: T, delay = DEFAULT_DELAY) {
    const paramsRef = useSyncedRef({ cb, delay })
-   
-   const debouncedCb = useCallback(() => debouncedFnWrapper(paramsRef.current.cb, paramsRef.current.delay), [])
+
+   const debouncedCb = useCallback(debouncedFnWrapper(paramsRef.current.cb, paramsRef.current.delay), [])
    return debouncedCb
 }
 
-export function debouncedFnWrapper<T extends (...args: any[]) => any>(cb: T, delay: number = 300) {
+export function debouncedFnWrapper<T extends (...args: any[]) => any>(cb: T, delay = DEFAULT_DELAY) {
    let timerId: NodeJS.Timeout
-
    return (...args: Parameters<typeof cb>) => {
       if (timerId) {
          clearTimeout(timerId)
       }
-      timerId = setTimeout(() => cb.call(args), delay)
+      timerId = setTimeout(() => cb.call(null, ...args), delay)
    }
 }
