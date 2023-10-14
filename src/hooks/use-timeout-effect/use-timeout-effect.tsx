8ac89333a43d5bd8b727e999d52c25d2030e32a8
Ashish-simpleCoder <ashishprajapati357753357@gmail.com>
2023-10-14 19:43:58 +0530

    [feat]: add clearTimer and restartTime function in timeout and interval hook
    - Add clearTimer and restartTimer functions in use-interval-effect and use-timeout-effect hook

--- src/hooks/use-timeout-effect/use-timeout-effect.tsx
+++ src/hooks/use-timeout-effect/use-timeout-effect.tsx
@@ -1,13 +1,23 @@
-import { useEffect } from 'react'
+import { useCallback, useEffect, useRef } from 'react'
 import useSyncedRef from '../use-synced-ref/use-synced-ref'
 
 export default function useTimeoutEffect(cb: () => void, delay = 0) {
    let paramsRef = useSyncedRef({ cb, delay })
+   const timeoutId = useRef<NodeJS.Timeout>()
+
+   const clearTimer = useCallback(() => {
+      clearInterval(timeoutId.current)
+   }, [])
+
+   const restartTimer = useCallback(() => {
+      clearTimer()
+      timeoutId.current = setInterval(paramsRef.current.cb, paramsRef.current.delay)
+   }, [])
 
    useEffect(() => {
-      let timerId = setTimeout(paramsRef.current.cb, paramsRef.current.delay)
-      return () => {
-         clearTimeout(timerId)
-      }
+      timeoutId.current = setTimeout(paramsRef.current.cb, paramsRef.current.delay)
+      return clearTimer
    }, [])
+
+   return {clearTimer, restartTimer}
 }
