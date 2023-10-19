d5527b22b7d1cfefb4771cd5b140bad668d54a4c
Ashish-simpleCoder <ashishprajapati357753357@gmail.com>
2023-10-19 20:40:11 +0530

    [feat]: add use-counter hook

---
+++ src/utils/capitalizeFirstLetter.ts
@@ -0,0 +1,7 @@
+export function capitalizeFirstLetter<K extends string = ''>(str: K): Capitalize<typeof str>{
+   const firstLetter = str.charAt(0)
+   const firstLetterCap = firstLetter.toUpperCase()
+   const remainingLetters = str.slice(1)
+   const capitalizedWord = firstLetterCap + remainingLetters
+   return capitalizedWord as Capitalize<typeof str>
+}
