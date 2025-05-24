0754d8c581cc9a9df5e44f9a3a21625c7660cfb4
Ashish-simpleCoder <ashishprajapati357753357@gmail.com>
2025-05-24 12:38:31 +0530

    feat: update api for use-intersection-hook
    - use object based params instead of sequential params
    - update the test

--- src/lib/use-intersection-observer/index.tsx
+++ src/lib/use-intersection-observer/index.tsx
@@ -1,25 +1,26 @@
-import type { RefObject } from 'react'
-import type { Prettify } from '../../types'
+import type { IntersectionOptions, IntersectionObserverTarget, IsTargetIntersecting } from '../../types'
 
-import { useState, useEffect } from 'react'
-
-export type Target = HTMLElement | RefObject<HTMLElement> | (() => HTMLElement | null) | null
-
-type Options = {
-   mode?: 'lazy' | 'virtualized'
-} & IntersectionObserverInit
+import { useEffect, useState } from 'react'
 
 /**
  * @description
  *  A hook which provides a way for listening to the Intersection Observer event for given target.
  *
- *  It takes an array of targets and returns an array of boolean values which represents whether the targets are intersecting the screen or not.
+ *  It takes an array of targets and returns an array of boolean values which represents whether the targets are intersecting to the screen or not.
  *
  * @see Docs https://classic-react-hooks.vercel.app/hooks/use-intersection-observer.html
  */
-export default function useInterSectionObserver(targets: Target[], options: Prettify<Options> = {}): Array<boolean> {
+export default function useInterSectionObserver({
+   targets,
+   options = { only_trigger_once: true },
+   onIntersection,
+}: {
+   targets: IntersectionObserverTarget[]
+   options?: IntersectionOptions
+   onIntersection?: (target: Element) => void
+}): Array<IsTargetIntersecting> {
    const [visibilityStates, setVisiblilityStates] = useState(() => {
-      return new Array(targets.length).fill(false) as Array<boolean>
+      return new Array(targets.length).fill(false) as Array<IsTargetIntersecting>
    })
 
    const intersection_options: IntersectionObserverInit = {
@@ -28,33 +29,41 @@ export default function useInterSectionObserver(targets: Target[], options: Pret
       threshold: options.threshold,
    }
 
-   if (!options.mode) {
-      options.mode = 'lazy'
-   }
-
    useEffect(() => {
       if (!window.IntersectionObserver) {
          console.warn('IntersectionObserver is not available.')
          return
       }
+      options = {
+         only_trigger_once: true,
+         ...options,
+      }
       const io = new IntersectionObserver((entries) => {
          entries.forEach((entry) => {
-            const entry_idx = entry.target.getAttribute('idx')
+            const entry_idx = Number(entry.target.getAttribute('idx') ?? -1) // assign -1 to ignore the observation
+
             if (entry.isIntersecting) {
                setVisiblilityStates((_visibilityState) => {
-                  if (entry_idx == null) return _visibilityState
+                  if (entry_idx == -1) return _visibilityState
 
-                  _visibilityState[+entry_idx] = true
+                  _visibilityState[entry_idx] = true
                   return [..._visibilityState]
                })
-               if (options.mode == 'lazy') {
+               // unobserve the target in each iteration if only_trigger_once is true
+               if (options.only_trigger_once == true) {
                   io.unobserve(entry.target)
+               } else if (Array.isArray(options.only_trigger_once)) {
+                  if (entry_idx != -1 && options.only_trigger_once[entry_idx] == true)
+                     // if for an specific element, only_trigger_once is true, then unobserve it
+                     io.unobserve(entry.target)
                }
+               // callback to run after element is visible on screen
+               onIntersection?.(entry.target)
             } else {
                setVisiblilityStates((_visibilityState) => {
-                  if (entry_idx == null) return _visibilityState
+                  if (entry_idx == -1) return _visibilityState
 
-                  _visibilityState[+entry_idx] = false
+                  _visibilityState[entry_idx] = false
                   return [..._visibilityState]
                })
             }
@@ -63,21 +72,13 @@ export default function useInterSectionObserver(targets: Target[], options: Pret
 
       targets.forEach((element, idx) => observer(element, idx))
 
-      function observer(element: Target, idx: number) {
-         let target: HTMLElement | null = null
-
+      function observer(element: IntersectionObserverTarget, idx: number) {
          try {
-            if (element && 'current' in element) {
-               target = element.current
-            } else if (typeof element == 'function') {
+            if (typeof element == 'function') {
                const ele = element()
-               target = ele
-            } else {
-               target = element
-            }
-            if (target) {
-               target.setAttribute('idx', idx.toString())
-               io.observe(target)
+               if (!ele || !(ele instanceof Element)) return
+               ele.setAttribute('idx', idx.toString())
+               io.observe(ele)
             }
          } catch (err) {
             console.warn(err)
--- src/types/index.ts
+++ src/types/index.ts
@@ -7,3 +7,11 @@ export interface EvOptions extends AddEventListenerOptions {
    shouldInjectEvent?: boolean | any
 }
 export type EvHandler = (event: Event) => void
+
+
+// use-intersection type
+export interface IntersectionOptions extends IntersectionObserverInit {
+   only_trigger_once?: boolean | Array<boolean>
+}
+export type IntersectionObserverTarget = () => HTMLElement | null
+export type IsTargetIntersecting = boolean
