8305b6de969664d022f6907a5aff5884e385e164
Ashish-simpleCoder <ashishprajapati357753357@gmail.com>
2025-10-15 22:24:54 +0530

    fix: missing export for use-multi-intersection-observer hook

---
+++ .changeset/spotty-jeans-carry.md
@@ -0,0 +1,5 @@
+---
+'classic-react-hooks': minor
+---
+
+- Fix missing import for `useMultipleIntersectionObserver` hook
--- src/index.tsx
+++ src/index.tsx
@@ -26,3 +26,4 @@ export { default as useOnMountEffect } from './lib/use-on-mount-effect'
 export { default as useCounter } from './lib/use-counter'
 
 export { default as useInterSectionObserver } from './lib/use-intersection-observer'
+export { default as useMultipleIntersectionObserver } from './lib/use-multi-intersection-observer'
